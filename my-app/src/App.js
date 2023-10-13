2155c98574ec2a0d2f1c0b0e24f3816d4be58e64
SK <test@gmail.com>
2023-10-14 00:33:10 +1000

    Created Components, passed props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 function App() {
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <Header shopname="Nature's Basket" />
+      <Content fruitList={fruitObjList}/>
+      <Footer year={new Date().getFullYear()} />
+    </section>
   );
 }
+
+const fruitList = ["Apple", "Oranges", "Guava", "Watermelom"];
+
+const fruitObjList = fruitList.map(
+  (fruit, i) => ({
+    id: i,
+    title: fruit
+  }
+));
+
+function Content(props) {
+  return (
+    <ul>
+      {props.fruitList.map((fruit) => (
+        <li key={fruit.id}>{fruit.title}</li>
+      ))}
+      </ul>
+  )
+}
+
+function Header(props) {
+  return <header>
+    <h1>{props.shopname}</h1>
+  </header>
+}
+
+function Footer(props) {
+  return <footer>
+    <h5>Copyright {props.year}</h5>
+  </footer>
+}
+
 
 export default App;
